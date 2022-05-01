23e49abb79d0c66211b3f40f3a93a873755e2465
Adedipupo <ajokanola@terragonltd.com>
2022-05-01 06:51:56 +0100

    topbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Topbar from "./components/topbar/Topbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome home</h1>
+    <div>
+      <Topbar />
     </div>
   );
 }
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./topbar.css"
+
+export default function  () {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+          <div className="topbarLeft">left</div>
+          <div className="topbarRight">right</div>
+      </div>
+    </div>
+  )
+}
