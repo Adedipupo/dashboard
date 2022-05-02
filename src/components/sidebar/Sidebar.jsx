f08632cc7922b4a2ba1f751d902b4921ec8170ba
Adedipupo <ajokanola@terragonltd.com>
2022-05-02 15:36:33 +0100

    sidebar wip

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
-import { LineStyle, Timeline, TrendingUp } from "@material-ui/icons";
+import { ModalManager } from "@material-ui/core";
+import { BrokenImageTwoTone, Chat, CropOriginalOutlined, Feedback, GraphicEq, LineStyle, Mail, Money, MoneyOutlined, PeopleOutlineTwoTone, PeopleTwoTone, ReportOff, ReportTwoTone, Timeline, TrendingUp } from "@material-ui/icons";
 import "./sidebar.css";
 
 export default function Sidebar() {
@@ -23,19 +24,57 @@ export default function Sidebar() {
               </ul>
           </div>
           <div className="sideMenu">
-              <h3 className="sidebarTitle">Dashboard</h3>
+              <h3 className="sidebarTitle">Quick Menu</h3>
               <ul className="sidebarList">
                   <li className="sidebarListItem">
-                    <LineStyle className="sidebarIcon"/>
-                    Home
+                    <PeopleOutlineTwoTone className="sidebarIcon"/>
+                    Users
+                  </li>
+                  <li className="sidebarListItem">
+                    <BrokenImageTwoTone className="sidebarIcon"/>
+                    Products
+                  </li>
+                  <li className="sidebarListItem">
+                    <MoneyOutlined className="sidebarIcon"/>
+                    Transactions
+                  </li>
+                  <li className="sidebarListItem">
+                    <GraphicEq className="sidebarIcon"/>
+                    Reports
+                  </li>
+              </ul>
+          </div>
+          <div className="sideMenu">
+              <h3 className="sidebarTitle">Notifications</h3>
+              <ul className="sidebarList">
+                  <li className="sidebarListItem">
+                    <Mail className="sidebarIcon"/>
+                    Mail
+                  </li>
+                  <li className="sidebarListItem">
+                    <Feedback className="sidebarIcon"/>
+                    Feedback
+                  </li>
+                  <li className="sidebarListItem">
+                    <Chat className="sidebarIcon"/>
+                    Messages
+                  </li>
+              </ul>
+          </div>
+          <div className="sideMenu">
+              <h3 className="sidebarTitle">Staff</h3>
+              <ul className="sidebarList">
+                  <li className="sidebarListItem">
+                    <CropOriginalOutlined className="sidebarIcon"/>
+                    Manage
                   </li>
                   <li className="sidebarListItem">
                     <Timeline className="sidebarIcon"/>
                     Analytics
                   </li>
                   <li className="sidebarListItem">
-                    <TrendingUp className="sidebarIcon"/>
-                    Sales
+                    <ReportOff className="sidebarIcon"/>
+                    Reports
                   </li>
               </ul>
           </div>
