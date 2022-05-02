a722115282c446c0d909842d0c067f8a33f4c82e
Adedipupo <ajokanola@terragonltd.com>
2022-05-02 14:59:58 +0100

    sidebar wip

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import "./sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+          <div className="sideMenu">
+              <h3 className="sideTitle">Dashboard</h3>
+          </div>
+      </div>
+    </div>
+  )
+}
